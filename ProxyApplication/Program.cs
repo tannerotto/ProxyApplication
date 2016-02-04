dfe4fd77d6e20f50c6b272ee23b42e82c6559057
arctair <jtyler_@outlook.com>
2016-02-04 11:24:50 -0600

    added basic console interface code

--- ProxyApplication/Program.cs
+++ ProxyApplication/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProxyApplication
@@ -10,6 +11,60 @@ namespace ProxyApplication
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Initializing proxy application");
+
+            Console.Write("Proxy application loaded. ");
+
+            if (Array.Exists(args, arg =>
+                arg.ToLower().Equals("--client") ||
+                arg.ToLower().Equals("-c")))
+            {
+                Console.WriteLine();
+                Client(args);
+            }
+            else
+            {
+                Console.WriteLine("Type help to view all commands");
+            }//Server(args);
+
+
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                switch(line.ToLower())
+                {
+                    case "client":
+                        Client(line.Length >= 6 ? line.Substring(6).Split(' ') : new string[0]);
+                        break;
+                    case "server":
+                        Server(line.Length >= 6 ? line.Substring(6).Split(' ') : new string[0]);
+                        break;
+                    default:
+                        Help(new string[0]);
+                        break;
+                }
+            }
+        }
+
+        static void Client(string[] args)
+        {
+            Console.WriteLine("Client started");
+            Console.WriteLine("Client shutting down");
+        }
+
+        static void Server(string[] args)
+        {
+            Console.WriteLine("Server started.");
+            Console.WriteLine("Server shutting down.");
+        }
+
+        static void Help(string[] args)
+        {
+            Console.WriteLine(
+                "Valid commands:\n" +
+                "client - Start the proxy debug client\n" +
+                "server - Start the proxy server\n");
         }
     }
 }
